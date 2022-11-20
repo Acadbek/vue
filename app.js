f3d1262f9be60e7784a62607a1a0b770424177db
Asadbek <a.nosirjonov@uic.group>
2022-11-21 00:34:17 +0500

    keybord events

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
 new Vue({
   el: "#vue-app",
   data: {
-    age: 21,
-    x: 0,
-    y: 0,
+    value: 0,
   },
   methods: {
-    add: function () {
-      this.age++;
-    },
-    subtract: function () {
-      this.age--;
-    },
-    updateXY: function (event) {
-      console.log(event);
-      (this.x = event.offsetX), (this.y = event.offsetY);
+    update: function (t) {
+      console.log("entered", t.target.value);
+      t.target.value = "";
     },
   },
 });
