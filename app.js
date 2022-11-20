e0523fd11287b730c3e3362aafbf40767d3d47ca
Asadbek <a.nosirjonov@uic.group>
2022-11-21 02:31:17 +0500

    dynamic css classes

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
 new Vue({
   el: "#vue-app",
   data: {
-    a: 0,
-    b: 0,
-    age: 20,
+    active: false,
+    active2: false,
   },
-  methods: {},
   computed: {
-    addToA: function () {
-      console.log("A");
-      return this.age + this.a;
-    },
-    addToB: function () {
-      console.log("B");
-      return this.age + this.b;
+    compClasses: function () {
+      return {
+        active: this.active,
+        active2: this.active2,
+      };
     },
   },
 });
