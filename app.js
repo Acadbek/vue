600b3429da59fd251d89da6b9a9dfc3954f35477
Asadbek <a.nosirjonov@uic.group>
2022-11-21 01:16:41 +0500

    computed properties

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 new Vue({
   el: "#vue-app",
   data: {
-    name: "",
-    age: "",
+    a: 0,
+    b: 0,
+    age: 20,
   },
   methods: {},
+  computed: {
+    addToA: function () {
+      console.log("A");
+      return this.age + this.a;
+    },
+    addToB: function () {
+      console.log("B");
+      return this.age + this.b;
+    },
+  },
 });
